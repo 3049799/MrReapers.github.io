4229e0d36f8267c2003ccd80cb9682f4c5c4fb45
MrReaper <achi49799@adams12.org>
2026-04-19 00:35:35 +0000

    Delete MrReapers.js

--- MrReapers.js
+++
@@ -1,9 +0,0 @@
-    function showPage(pageId) {
-      const pages = document.querySelectorAll('.page');
-      pages.forEach(page => page.classList.remove('active'));
-
-      const selected = document.getElementById(pageId);
-      if (selected) {
-        selected.classList.add('active');
-      }
-    }
